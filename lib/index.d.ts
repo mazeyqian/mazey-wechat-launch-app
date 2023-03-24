46f40c20bbde6f45c06b8278e7cd5ead6e58669f
Mazey Chu <mazey@mazey.net>
2023-03-24 13:51:21 +0800

    chore: build first

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,16 +1,31 @@
 /**
- * @author: Mazey Chu
- */
-/**
- * Foo foo foo, Foo description.
+ * Launch APP
  *
- * @returns {Boolean} true or false
+ * @param {string} jqSelector Example: '#mazey-wx-btn-report'
+ * @param {string} launchInBrowserClassName Example: 'mazey-launch-in-browser-btn'
+ * @param {string} genTagPrefixStr Example: 'mazey-launch-btn-'
+ * @param {string} launchShowWeixinToBrowserClassName Example: 'mazey-wx-to-browser'
+ * @param {string} launchBtnClassName Example: 'mazey-btn'
+ * @param {string} schemeUrl Example: 'mazey://launch/page'
+ * @param {string} wexinServiceAccountAppId Example: 'wx1234'
+ * @param {string} openPlatformMobileAppId Example: 'wx5678'
+ * @param {boolean} isConClosed
+ * @param {function} canLaunchApp
+ * @param {function} canOpenAppFromWeixin
+ * @returns {void}
  */
-export declare function foo(): boolean;
-/**
- * Bar bar bar, Bar description.
- *
- * @returns {Boolean} true or false
- */
-export declare function bar(): boolean;
+declare const _default: (options?: {
+    jqSelector?: string | undefined;
+    launchInBrowserClassName?: string | undefined;
+    genTagPrefixStr?: string | undefined;
+    launchShowWeixinToBrowserClassName?: string | undefined;
+    launchBtnClassName?: string | undefined;
+    schemeUrl?: string | undefined;
+    wexinServiceAccountAppId?: string | undefined;
+    openPlatformMobileAppId?: string | undefined;
+    isConClosed?: boolean | undefined;
+    canLaunchApp?: ((data: any) => boolean) | undefined;
+    canOpenAppFromWeixin?: (() => boolean) | undefined;
+}) => void;
+export default _default;
 //# sourceMappingURL=index.d.ts.map
