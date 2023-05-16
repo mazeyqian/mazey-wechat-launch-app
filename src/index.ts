7272baef069fe1b0b997b3a675f2e4cb05a44e19
Mazey Chu <mazey@mazey.net>
2023-05-16 16:32:48 +0800

    style: remove space

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const defaultOptions = {
   genTagPrefixStr: 'mazey-launch-app-btn-prefix-',
   launchShowWeixinToBrowserClassName: 'mazey-launch-app-wx-to-browser',
   launchBtnClassName: 'mazey-launch-app-inner-btn',
-  launchBtnStyle: '', // 'top: 0; right: 0; bottom: 0; left: 0;' +
+  launchBtnStyle: '', // 'top:0;right:0;bottom:0;left:0;' +
   launchBtnText: 'Launch App',
   launchErrorLink: '',
   canContinuousUpdating: false,
